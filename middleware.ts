cf0e6f3e99cd937a4f3656a74b64eca1408750e5
Wolmer <contact@wolmer.me>
2024-05-13 01:46:37 +0530

    refactor: dashboard in root
    This commit removes the following unused files:
    - app/(dashboard)/dashboard/orders/page.tsx
    - app/(dashboard)/dashboard/page.tsx
    - app/(dashboard)/dashboard/products/page.tsx
    - app/(dashboard)/dashboard/settings/page.tsx
    - app/(dashboard)/layout.tsx
    - app/(root)/(routes)/page.tsx
    
    These files are no longer needed in the project and have been deleted to improve code organization and reduce unnecessary code.

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 import { auth } from '@/auth';
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
 
 export default auth((req) => {
