1f6b3dd033e165a70dbbda46bfd9387305c3f3fa
Wolmer <contact@wolmer.me>
2024-05-28 16:22:42 +0530

    refactor: Update CardHeader styling for responsive layout
    - Modify the CardHeader component in the DashboardPage to use flexbox properties for responsive layout
    - Change the flex direction to column-reverse on medium-sized screens to ensure proper alignment
    
    Note: This commit message follows the established convention of starting with a verb in the imperative form and providing a concise summary of the changes made.

--- app/(root)/(dashboard)/dashboard/page.tsx
+++ app/(root)/(dashboard)/dashboard/page.tsx
@@ -21,7 +21,7 @@ const DashboardPage = async () => {
       <Header title="Overview" description={`Welcome back, ${session?.user?.name} 👋`}></Header>
       <section className="mx-auto grid max-w-6xl grid-cols-3 gap-4 px-10 md:p-0">
         <Card>
-          <CardHeader className="flex flex-row items-center justify-between text-sm">
+          <CardHeader className="flex flex-col-reverse items-center justify-between text-sm md:flex-row">
             <CardTitle className="text-base font-medium">Total Revenue</CardTitle>
             <DollarSign className="text-muted-foreground" size={26} />
           </CardHeader>
@@ -30,7 +30,7 @@ const DashboardPage = async () => {
           </CardContent>
         </Card>
         <Card>
-          <CardHeader className="flex flex-row items-center justify-between text-sm">
+          <CardHeader className="flex flex-col-reverse items-center justify-between text-sm md:flex-row">
             <CardTitle className="text-base font-medium">Sales</CardTitle>
             <PackageIcon className="text-muted-foreground" />
           </CardHeader>
@@ -39,7 +39,7 @@ const DashboardPage = async () => {
           </CardContent>
         </Card>
         <Card>
-          <CardHeader className="flex flex-row items-center justify-between text-sm">
+          <CardHeader className="flex flex-col-reverse items-center justify-between text-sm md:flex-row">
             <CardTitle className="text-base font-medium">Products in Store</CardTitle>
             <ShirtIcon className="text-muted-foreground" />
           </CardHeader>
