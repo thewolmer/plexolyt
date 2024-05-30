cdd94e6701007453e4ecef5bddc698b3484424f7
Wolmer <contact@wolmer.me>
2024-05-30 22:30:14 +0530

    feat: Add Related Products Carousel component for displaying related products
    - Create a new file, RelatedCarousel.tsx, in the product components directory
    - Import necessary dependencies and components for the carousel functionality
    - Implement the RelatedProducts component with async/await for fetching related products
    - Render the carousel with the fetched data and display product cards
    - Add previous and next buttons for carousel navigation
    
    Note: This commit message follows the established convention of starting with a verb in the imperative form and providing a concise summary of the changes made.

---
+++ app/(root)/(client)/product/[productId]/components/RelatedCarousel.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { getRelatedProducts } from '@/actions/products.client';
+import { ProductCard } from '@/components/ProductCard';
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
+
+interface Props {
+  categoryId: string;
+  currentProductId: string;
+}
+
+export const RelatedProducts = async ({ categoryId, currentProductId }: Props) => {
+  const featuredProducts = await getRelatedProducts(categoryId, currentProductId);
+  if (!featuredProducts || featuredProducts.data?.length === 0)
+    return <div className="text-center">No related products found</div>;
+  return (
+    <Carousel
+      opts={{
+        align: 'start',
+      }}
+      className="w-full max-w-sm md:mx-auto md:max-w-6xl"
+    >
+      <CarouselContent className="">
+        {featuredProducts?.data?.map((data, index) => (
+          <CarouselItem key={index} className="basis-1/2 pl-1 lg:basis-1/3">
+            <div className="flex items-center justify-center p-1">
+              <ProductCard product={data} />
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+};
